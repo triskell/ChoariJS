cb62b71cb16fa72611194029ea727c3ac4a4eb47
tom <boquello-git@yahoo.fr>
2012-11-03 11:44:10 +0100

    License change, quick fix, and name change
    License update, file name changed to fit versions and minor bug fixed.

--- choari-0.0.1.js
+++ choari-0.0.1.js
@@ -1,3 +1,26 @@
+/***********************************************************************************************
+
+	ChoariJS - A Javacsript game library
+	Original source at https://github.com/triskell/ChoariJS
+	
+	Copyright(C)Thomas Abot, 2012.
+		
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*************************************************************************************************/
+
 var game = {
 
 	canvas : null,
@@ -5,10 +28,9 @@ var game = {
 	/**********
 	*
 	*	Initializes the library and the canvas.
-	*	Let width and/or height undefined to fit the canvas to the screen size.
+	*	Let width and/or height undefined to fit the canvas to the screen size (fullscreen).
 	*
 	*	Parameters :	canvas	: The canvas element.
-	*					mode	: "fullscreen" to set the canvas fullscreen, else "windowed".
 	*					width	: The width of the canvas. Precise the css unit of this attribute.
 	*					height	: The width of the canvas. Precise the css unit of this attribute.	
 	*
@@ -28,7 +50,7 @@ var game = {
 
 		//set fullscreen mode
 		if (mode.toLowerCase() === "fullscreen") {
-			this.setFullScreen(true, canvas);
+			this.setFullScreen(canvas, true);
 		}
 	},
 
